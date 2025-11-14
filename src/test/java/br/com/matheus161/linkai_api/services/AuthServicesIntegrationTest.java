8d761caa6332f0ebf9adeb0ae7c36125016bcb62
Matheus Lima <matheeus161@gmail.com>
2025-11-14 20:06:51 -0300

    test: add `/auth/register` validations test cases

--- src/test/java/br/com/matheus161/linkai_api/services/AuthServicesIntegrationTest.java
+++ src/test/java/br/com/matheus161/linkai_api/services/AuthServicesIntegrationTest.java
@@ -51,4 +51,88 @@ public class AuthServicesIntegrationTest {
                 .andExpect(jsonPath("$.email").value("email@test.com"))
                 .andExpect(jsonPath("$.token").exists());
     }
+
+    @Test
+    @DisplayName("should throw an Exception when user email already exists")
+    void registerCase2() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto("name", "email@test.com", "Senh@123456");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isConflict())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("User already exists"));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when name is not present")
+    void registerCase3() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto(null, "email@test.com", "Senh@123456");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Erro na validação"))
+                .andExpect(jsonPath("$.errors[0].field").value("name"))
+                .andExpect(jsonPath("$.errors[0].message").value("O nome é obrigatório"));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when email is not present")
+    void registerCase4() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto("name", null, "Senh@123456");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Erro na validação"))
+                .andExpect(jsonPath("$.errors[0].field").value("email"))
+                .andExpect(jsonPath("$.errors[0].message").value("O e-mail é obrigatório"));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when email is invalid")
+    void registerCase5() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto("name", "invalid-email", "Senh@123456");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Erro na validação"))
+                .andExpect(jsonPath("$.errors[0].field").value("email"))
+                .andExpect(jsonPath("$.errors[0].message").value("E-mail inválido"));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when password is not present")
+    void registerCase6() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto("name", "email@test.com", "senha-invalida");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Erro na validação"))
+                .andExpect(jsonPath("$.errors[0].field").value("password"))
+                .andExpect(jsonPath("$.errors[0].message").value("Senha inválida. Deve conter " +
+                        "pelo menos 8 caracteres, incluindo letra maiúscula, minúscula e número."));
+    }
 }
