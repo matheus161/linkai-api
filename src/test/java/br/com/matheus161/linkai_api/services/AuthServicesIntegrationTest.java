e9a12c61ec7cf446dde3e5e2edf1a3f6bb144f04
Matheus Lima <matheeus161@gmail.com>
2025-11-14 20:25:49 -0300

    test: add `/auth/login` tests validations

--- src/test/java/br/com/matheus161/linkai_api/services/AuthServicesIntegrationTest.java
+++ src/test/java/br/com/matheus161/linkai_api/services/AuthServicesIntegrationTest.java
@@ -1,5 +1,6 @@
 package br.com.matheus161.linkai_api.services;
 
+import br.com.matheus161.linkai_api.dto.LoginRequestDto;
 import br.com.matheus161.linkai_api.dto.RegisterRequestDto;
 import br.com.matheus161.linkai_api.repositories.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -134,5 +135,95 @@ public class AuthServicesIntegrationTest {
                 .andExpect(jsonPath("$.errors[0].field").value("password"))
                 .andExpect(jsonPath("$.errors[0].message").value("Senha inválida. Deve conter " +
                         "pelo menos 8 caracteres, incluindo letra maiúscula, minúscula e número."));
+    }
+
+    @Test
+    @DisplayName("should login successfully when everything is ok")
+    void loginCase1() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto("name", "email@test.com", "Senh@123456");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andExpect(status().isOk());
+
+        LoginRequestDto loginRequestDto = new LoginRequestDto("email@test.com",  "Senh@123456");
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequestDto)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("name"))
+                .andExpect(jsonPath("$.token").exists());
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when user not exists")
+    void loginCase2() throws Exception {
+        LoginRequestDto loginRequestDto = new LoginRequestDto("email@test.com",  "Senh@123456");
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequestDto)))
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("User not found"))
+                .andExpect(jsonPath("$.token").doesNotExist());
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when credential are invalid")
+    void loginCase3() throws Exception {
+        RegisterRequestDto requestBody = new RegisterRequestDto("name", "email@test.com", "Senh@123456");
+
+        mockMvc.perform(post("/auth/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(requestBody)))
+                .andExpect(status().isOk());
+
+        LoginRequestDto loginRequestDto = new LoginRequestDto("email@test.com",  "invalid-password");
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequestDto)))
+                .andDo(print())
+                .andExpect(status().isInternalServerError())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Invalid credentials"));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when email is not present")
+    void loginCase4() throws Exception {
+        LoginRequestDto loginRequestDto = new LoginRequestDto(null,  "invalid-password");
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequestDto)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Erro na validação"))
+                .andExpect(jsonPath("$.errors[0].field").value("email"))
+                .andExpect(jsonPath("$.errors[0].message").value("O e-mail é obrigatório"));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when passowrd is not present")
+    void loginCase5() throws Exception {
+        LoginRequestDto loginRequestDto = new LoginRequestDto("email@test.com",  null);
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequestDto)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.message").value("Erro na validação"))
+                .andExpect(jsonPath("$.errors[0].field").value("password"))
+                .andExpect(jsonPath("$.errors[0].message").value("A senha é obrigatória"));
     }
 }
