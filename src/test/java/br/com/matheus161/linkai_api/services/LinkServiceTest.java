504997df5500ff55dfc4f78943221f5145f67343
Matheus Lima <matheeus161@gmail.com>
2025-11-25 21:04:03 -0300

    test: add `should throw an Exception when user not exists` and `should throw an Exception when link already exists` tests

--- src/test/java/br/com/matheus161/linkai_api/services/LinkServiceTest.java
+++ src/test/java/br/com/matheus161/linkai_api/services/LinkServiceTest.java
@@ -1,8 +1,11 @@
 package br.com.matheus161.linkai_api.services;
 
+import br.com.matheus161.linkai_api.domain.link.Link;
 import br.com.matheus161.linkai_api.domain.user.User;
 import br.com.matheus161.linkai_api.dto.CreateLinkRequestDto;
 import br.com.matheus161.linkai_api.dto.CreateLinkResponseDto;
+import br.com.matheus161.linkai_api.exception.LinkAlreadyExistsException;
+import br.com.matheus161.linkai_api.exception.UserNotFoundException;
 import br.com.matheus161.linkai_api.infra.security.UrlIdGeneratorService;
 import br.com.matheus161.linkai_api.repositories.LinkRepository;
 import br.com.matheus161.linkai_api.repositories.UserRepository;
@@ -17,6 +20,7 @@ import org.mockito.MockitoAnnotations;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.Mockito.*;
 
@@ -78,5 +82,44 @@ public class LinkServiceTest {
                         link.getOriginal_link().equals(original_link) &&
                         link.getRedirect_id().equals(redirect_id)
         ));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when user not exists")
+    void createCase2() {
+        when(userRepository.findById(user_id)).thenReturn(Optional.empty());
+
+        // Act
+        CreateLinkRequestDto request = new CreateLinkRequestDto(title, description, original_link, user_id);
+        UserNotFoundException exception = assertThrows(UserNotFoundException.class, () -> linkService.create(request));
+
+        assertEquals("User not found", exception.getMessage());
+
+        verify(linkRepository, never()).findByTitleOrOriginalLink(anyString(), anyString());
+        verify(urlIdGeneratorService, never()).generate();
+        verify(linkRepository, never()).save(any(Link.class));
+    }
+
+    @Test
+    @DisplayName("should throw an Exception when link already exists")
+    void createCase3() {
+        User user = new User("name", "email", "Senha@123456");
+        when(userRepository.findById(user_id)).thenReturn(Optional.of(user));
+
+        Link link = new Link(title, description, original_link, redirect_id, user);
+        when(linkRepository.findByTitleOrOriginalLink(title, original_link)).thenReturn(Optional.of(link));
+
+        // Act
+        CreateLinkRequestDto request = new CreateLinkRequestDto(title, description, original_link, user_id);
+
+        LinkAlreadyExistsException exception = assertThrows(LinkAlreadyExistsException.class,
+                () -> linkService.create(request));
+
+        assertEquals("Link already exists", exception.getMessage());
+
+        verify(userRepository, times(1)).findById(user_id);
+        verify(linkRepository, times(1)).findByTitleOrOriginalLink(anyString(), anyString());
+        verify(urlIdGeneratorService, never()).generate();
+        verify(linkRepository, never()).save(any(Link.class));
     }
 }
